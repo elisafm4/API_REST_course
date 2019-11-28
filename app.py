1eda7cc29942549fb430f09f4b83be3ac450cc1d
elisafm4 <44262088+elisafm4@users.noreply.github.com>
2019-11-28 14:23:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,10 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # don¡t track changes in 
 app.secret_key = 'jose'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all() # if the db set on sqlite doesn't exists, create it
-
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Item, '/item/<string:name>')
